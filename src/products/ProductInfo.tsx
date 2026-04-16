7e1c623fb38aa0d3420b0360aead37a65e48f07d
Roco-LEE <aowlrtmd@gmail.com>
2026-04-16 19:16:12 +0900

    second commit

---
+++ src/products/ProductInfo.tsx
@@ -0,0 +1,36 @@
+
+import products from "../data/products.json"
+import { useParams } from "react-router-dom"
+
+import {imageMap} from "./ProductList"
+
+const ProductInfo = () => {
+    const { id } = useParams();
+
+    // id로 상품 찾기
+    const product = products.find((p: any) => p.id === Number(id))
+
+    // id가 없는 경우
+    if(!product){
+        return <p>상품을 찾을 수 없습니다.</p>
+    }
+
+    return(
+        <div className="product-info">
+            <h2>{product.name}</h2>
+            <div className="product-details">
+                <img
+                    src={imageMap[product.image]}
+                    alt={product.name}
+                    className="product-image"
+                />
+                <div className="product-content">
+                    <p>{product.description}</p>
+                    <p className="price">가격: {product.price}원</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ProductInfo
