4d92958019aa70549cc8820065675e0c34c71369
Roco-LEE <aowlrtmd@gmail.com>
2026-04-16 19:19:16 +0900

    config

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://kiyongee2.github.io/shopping/'
+  base: 'https://roco-lee.github.io/shopping/'
 })
